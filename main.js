a11bc7abdd573ec96d523cdd9765705e43565d4b
Kevin Wong <wongka90@gmail.com>
2016-07-14 16:17:12 +0800

    added checkbox

--- main.js
+++ main.js
@@ -22,77 +22,82 @@ function submitform(){
   var referral_code = document.getElementById('referralcode').value;
   var password = document.getElementById('password').value;
   var confirmPassword = document.getElementById('confirmPassword').value;
+  var checkbox_tnc = document.getElementById('accept_tnc');
   var messages = [];
-  if (type.trim() == "" ){
-    messages.push("Please select what you are registering as.");
-  }
-  if (fullname.trim() == "" && type.trim() != "Other"){
-    messages.push("Invalid/Blank full name");
-  }
-  // if (rid.trim() == ""){
-  //   messages.push("Invalid/Blank registration number");
-  // }
-  if (email.trim() == "" || email.indexOf("@") < 1 ){
-    messages.push("Invalid/Blank email");
-  }
-  if (password.trim() == "" ){
-    messages.push("Invalid/Blank password");
-  } else if (password.trim().length < 8) {
-    messages.push("Password needs to be at least 8 characters long");
-  }
-  if (confirmPassword.trim() != password.trim()){
-    messages.push("Passwords do not match");
-  }
-  if (mobile.trim() == "" || mobile.toString().length != 8 ){
-    messages.push("Invalid/Blank mobile phone");
-  }
-  if (postal.trim() == ""){
-    messages.push("Invalid/Blank home postal code");
-  }
-  // if (referral_code.trim() == "" ){
-  //   messages.push("Invalid/Blank referral code");
-  // }
-
-  if (messages.length > 0 ){
-    alert(messages.join('\n'));
-  } else {
-    loader.style.display = 'block';
-    disableScrolling();
-    var jsonParams = {
-      "name" : fullname,
-      "email" : email,
-      "mobilePhone" : mobile,
-      "uid" : rid,
-      "type" : type,
-      "postalCode" : postal,
-      "promoCode" : referral_code,
-      "password": password.trim(),
-      "cpassword": confirmPassword.trim()
+  if(checkbox_tnc.checked){
+    if (type.trim() == "" ){
+      messages.push("Please select what you are registering as.");
     }
-    var url = (window.location.hostname.indexOf('www.ebeepartners.com') > -1) ? 'https://api.ebeecare.com' : 'http://dev.ebeecare.com';
+    if (fullname.trim() == "" && type.trim() != "Other"){
+      messages.push("Invalid/Blank full name");
+    }
+    // if (rid.trim() == ""){
+    //   messages.push("Invalid/Blank registration number");
+    // }
+    if (email.trim() == "" || email.indexOf("@") < 1 ){
+      messages.push("Invalid/Blank email");
+    }
+    if (password.trim() == "" ){
+      messages.push("Invalid/Blank password");
+    } else if (password.trim().length < 8) {
+      messages.push("Password needs to be at least 8 characters long");
+    }
+    if (confirmPassword.trim() != password.trim()){
+      messages.push("Passwords do not match");
+    }
+    if (mobile.trim() == "" || mobile.toString().length != 8 ){
+      messages.push("Invalid/Blank mobile phone");
+    }
+    if (postal.trim() == ""){
+      messages.push("Invalid/Blank home postal code");
+    }
+    // if (referral_code.trim() == "" ){
+    //   messages.push("Invalid/Blank referral code");
+    // }
 
-    request
-      .post(url + '/api/registerPartner')
-      .send(jsonParams)
-      .end(function(err, res) {
-        if (err) {
-          loader.style.display = 'none';
-          alert("Error!");
-          enableScrolling();
-        } else {
-          loader.style.display = 'none';
-          // console.log(res);
-          if (res.body.status == 1) {
-              alert("Thank you for signing up as our healthcare professional!");
-              ga('send', 'event', 'account', 'registration', 'Nurse', 1);
-              window.location = "https://app.ebeecare.com/";
+    if (messages.length > 0 ){
+      alert(messages.join('\n'));
+    } else {
+      loader.style.display = 'block';
+      disableScrolling();
+      var jsonParams = {
+        "name" : fullname,
+        "email" : email,
+        "mobilePhone" : mobile,
+        "uid" : rid,
+        "type" : type,
+        "postalCode" : postal,
+        "promoCode" : referral_code,
+        "password": password.trim(),
+        "cpassword": confirmPassword.trim()
+      }
+      var url = (window.location.hostname.indexOf('www.ebeepartners.com') > -1) ? 'https://api.ebeecare.com' : 'http://dev.ebeecare.com';
+
+      request
+        .post(url + '/api/registerPartner')
+        .send(jsonParams)
+        .end(function(err, res) {
+          if (err) {
+            loader.style.display = 'none';
+            alert("Error!");
+            enableScrolling();
           } else {
-             alert("Oops, " + res.body.message + " \n Error(s): "+res.body.errors);
-             ga('send', 'event', 'account', 'registration', 'Nurse', 0);
+            loader.style.display = 'none';
+            // console.log(res);
+            if (res.body.status == 1) {
+                alert("Thank you for signing up as our healthcare professional!");
+                ga('send', 'event', 'account', 'registration', 'Nurse', 1);
+                window.location = "https://app.ebeecare.com/";
+            } else {
+               alert("Oops, " + res.body.message + " \n Error(s): "+res.body.errors);
+               ga('send', 'event', 'account', 'registration', 'Nurse', 0);
+            }
+            enableScrolling();
           }
-          enableScrolling();
-        }
-      });
+        });
+    }
+  }else{
+    alert("Please accept our Terms of Service and Privacy Policy before proceeding with account sign up.");
   }
 }
 
